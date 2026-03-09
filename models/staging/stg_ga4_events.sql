8a7dfaafe8aa26f806630f88f1460ef68c23bc9b
databiexpert <k.ergyun@gmail.com>
2026-03-09 18:00:35 +0100

    add stg_ga4_events staging model

---
+++ models/staging/stg_ga4_events.sql
@@ -0,0 +1,42 @@
+with source as (
+    select *
+    from {{ source('ga4', 'events') }}
+),
+
+renamed as (
+    select
+        -- Event fields
+        event_date,
+        timestamp_micros(event_timestamp) as event_timestamp,
+        event_name,
+        event_value_in_usd,
+
+        -- User fields
+        user_pseudo_id,
+        user_id,
+        timestamp_micros(user_first_touch_timestamp) as user_first_touch_timestamp,
+
+        -- Device fields
+        device.category as device_category,
+        device.operating_system as device_os,
+        device.browser as device_browser,
+        device.language as device_language,
+
+        -- Geo fields
+        geo.country as geo_country,
+        geo.region as geo_region,
+        geo.city as geo_city,
+
+        -- Traffic source fields
+        traffic_source.source as traffic_source,
+        traffic_source.medium as traffic_medium,
+        traffic_source.name as traffic_campaign,
+
+        -- Platform
+        platform,
+        stream_id
+
+    from source
+)
+
+select * from renamed
